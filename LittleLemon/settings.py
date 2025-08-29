b562e0a0bb27c384bf08dd0fb169442170045ab8
reda-salimi <salimireda16@gmail.com>
2025-08-29 20:54:41 +0100

    Add models in the models.py file and make migrations

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
